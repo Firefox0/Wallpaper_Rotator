7076678446e37cb030e0b1cb250345ed9c4981ad
Firefox0 <you@example.com>
2020-12-21 11:27:03 +0100

    renamed variables and adapted to module changes

--- Wallpaper_Rotator/wallpaper_rotator.py
+++ Wallpaper_Rotator/wallpaper_rotator.py
@@ -1,6 +1,5 @@
 import argparse
 import wallpaper
-import loader
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -8,12 +7,12 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--interval", type=int, help="Change wallpaper in this interval in seconds.")
     args = parser.parse_args()
     if not (args.directory and args.interval):
-        if loader.load_json("data.json"):
-            d = loader.load_json("data.json")
-            wallpaper = wallpaper.Wallpaper(d["directory"], d["interval"], d["ending_time"])
-            wallpaper.main_loop()
+        wp = wallpaper.Wallpaper()
+        if d := wp.load_json():
+            wp = wallpaper.Wallpaper(d["directory"], d["interval"], d["ending_time"])
+            wp.main_loop()
         else:
             print("If program gets called without arguments then there needs to be a json file.")
     else:
-        wallpaper = wallpaper.Wallpaper(args.directory, args.interval)
-        wallpaper.main_loop()
+        wp = wallpaper.Wallpaper(args.directory, args.interval)
+        wp.main_loop()
